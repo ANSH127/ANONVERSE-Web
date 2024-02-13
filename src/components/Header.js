bde978a53db6ecabe8247d6e70ef9c7aea81746f
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-02-13 18:13:49 +0530

    updating ProfileScreen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { UserCircleIcon } from "@heroicons/react/24/solid";
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Header() {
+    const navigate = useNavigate();
     return (
         <div className='flex justify-between items-center p-3 bg-white shadow-sm text-2xl font-bold'>
             <h1>AnonVerse</h1>
             <div>
-                <button className='hidden md:block p-2 rounded-full bg-gray-200'>
+                <button className='hidden md:block p-2 rounded-full bg-gray-200'
+                onClick={() => navigate('/profile')}
+                >
                     <UserCircleIcon className='h-8 w-8' />
                 </button>
             </div>
--- src/pages/ProfileScreen.js
+++ src/pages/ProfileScreen.js
@@ -1,7 +1,177 @@
 import React from 'react'
+import NavCard from '../components/NavCard'
+import ProfileCard from '../components/ProfileCard'
 
 export default function ProfileScreen() {
+  const [changeAvatar, setChangeAvatar] = React.useState(false)
+  const [avatar, setAvatar] = React.useState('')
   return (
-    <div>ProfileScreen</div>
+
+    <div className='grid-cols-1 grid md:grid-cols-4 gap-8  h-full p-4 m-2 fixed w-full' style={{ height: '100%' }}>
+      <div className='hidden md:block gap-4 col-span-1'>
+        {/* // profile card */}
+        <ProfileCard />
+        {/* // navigation card */}
+        <NavCard />
+
+
+      </div>
+      <div className='gap-4 col-span-2 h-full shadow-lg'>
+        {/* // main content */}
+        {!changeAvatar && <div className=' overflow-y-auto overflow-x-hidden'
+          style={{
+            scrollbarWidth: 'none', height: '100vh', paddingBottom: '100px',
+          }}>
+
+          {/* // user profile */}
+          <div className='items-center justify-center flex flex-col py-4'>
+            <img
+              src='./images/Avatar.jpg'
+              alt='profile'
+              className='rounded-full hover:border-2 border-blue-500'
+
+              width='150'
+              height='150'
+
+            />
+            <p className='text-red-500 text-xs cursor-pointer pt-2'
+              onClick={() => setChangeAvatar(!changeAvatar)}
+            >Change Avatar</p>
+
+            <h1 className='text-2xl py-2 font-bold'>John Doe</h1>
+          </div>
+          {/* // email address */}
+          <div className='flex justify-center gap-4 items-center space-y-4'>
+            <label className='text-gray-500 pt-2'>Email</label>
+
+            <input type='email' className='border border-gray-300 rounded-lg p-2 w-3/4' placeholder='Email Address' defaultValue="anshagrawal12348gmail.com" disabled />
+
+          </div>
+          {/* // logout button */}
+          <div className='flex justify-center mt-4'>
+            <button className='bg-blue-500 text-white px-4 py-2 rounded-md'>
+              Logout
+            </button>
+          </div>
+
+
+
+
+        </div>}
+
+        {/* // change avatar section */}
+        {changeAvatar &&
+          <div className=' overflow-y-auto overflow-x-hidden'
+            style={{
+              scrollbarWidth: 'none', height: '100vh', paddingBottom: '150px',
+            }}>
+            {/* // display all avtar images here */}
+            <div className='flex flex-wrap gap-4 justify-around items-center'>
+              <img
+                src='./images/Avatar/Avatar1.jpg'
+                alt='profile'
+                width='150'
+                height='150'
+                onClick={() => setAvatar('1')}
+                className={avatar === '1' ?
+                  'rounded-full border-4 border-blue-500 shadow-lg'
+                  :
+                  'rounded-full  hover:border-2 border-blue-500 '}
+
+
+
+              />
+              <img
+                src='./images/Avatar/Avatar2.jpg'
+                alt='profile'
+
+                width='150'
+                height='150'
+                onClick={() => setAvatar('2')}
+
+                className={avatar === '2' ?
+                  'rounded-full border-4 border-blue-500 shadow-lg'
+                  :
+                  'rounded-full  hover:border-2 border-blue-500 '}
+
+
+              />
+              <img
+                src='./images/Avatar/Avatar3.jpg'
+                alt='profile'
+
+                width='150'
+                height='150'
+                onClick={() => setAvatar('3')}
+
+                className={avatar === '3' ?
+                  'rounded-full border-4 border-blue-500 shadow-lg'
+                  :
+                  'rounded-full  hover:border-2 border-blue-500 '}
+
+
+              />
+              <img
+                src='./images/Avatar/Avatar4.jpg'
+                alt='profile'
+
+                width='150'
+                height='150'
+                onClick={() => setAvatar('4')}
+
+                className={avatar === '4' ?
+                  'rounded-full border-4 border-blue-500 shadow-lg'
+                  :
+                  'rounded-full  hover:border-2 border-blue-500 '}
+
+              />
+              <img
+                src='./images/Avatar/Avatar5.jpg'
+                alt='profile'
+
+                width='150'
+                height='150'
+                onClick={() => setAvatar('5')}
+
+                className={avatar === '5' ?
+                  'rounded-full border-4 border-blue-500 shadow-lg'
+                  :
+                  'rounded-full  hover:border-2 border-blue-500 '}
+
+              />
+              <img
+                src='./images/Avatar/Avatar6.jpg'
+                alt='profile'
+
+                width='150'
+                height='150'
+                onClick={() => setAvatar('6')}
+
+                className={avatar === '6' ?
+                  'rounded-full border-4 border-blue-500 shadow-lg'
+                  :
+                  'rounded-full  hover:border-2 border-blue-500 '}
+
+              />
+            </div>
+            {/* // save button */}
+            <div className='flex justify-center mt-4'>
+              <button className='bg-blue-500 text-white px-4 py-2 rounded-md'
+                onClick={() => setChangeAvatar(!changeAvatar)}
+              >
+                Save
+              </button>
+            </div>
+
+
+          </div>
+
+        }
+
+      </div>
+      <div className='hidden md:block'>
+        <h1>Section 3</h1>
+      </div>
+    </div>
   )
 }
