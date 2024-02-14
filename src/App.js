8a6feb3caccad019b8d8fd3dedce7ab779a2ee65
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-02-14 13:24:17 +0530

    adding signup and signin screen

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import ProfileScreen from './pages/ProfileScreen';
 import HomeScreen from './pages/HomeScreen';
 import Header from './components/Header';
 import BottemNavigation from './components/BottemNavigation';
+import SignInScreen from './pages/SignInScreen';
+import SignUpScreen from './pages/SignUpScreen';
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
           <Route path="/add" element={<AddConfessionScreen/>}/>
           <Route path="/your" element={<YourConfessionScreen/>}/>
           <Route path="/profile" element={<ProfileScreen/>}/>
+          <Route path="/signin" element={<SignInScreen/>}/>
+          <Route path="/signup" element={<SignUpScreen/>}/>
         </Routes>
       </Router>
     </>
---
+++ src/pages/SignUpScreen.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import NavCard from '../components/NavCard';
+import ProfileCard from '../components/ProfileCard';
+
+
+export default function SignUpScreen() {
+    return (
+
+        <div className='grid-cols-1 grid md:grid-cols-4 gap-8  h-full p-4 m-2 fixed w-full' style={{ height: '100%' }}>
+            <div className='hidden md:block gap-4 col-span-1'>
+                <ProfileCard />
+                <NavCard />
+            </div>
+            <div className='gap-4 col-span-2 h-full shadow-lg mr-5'>
+                <div className=' overflow-y-auto overflow-x-hidden'
+                    style={{
+                        scrollbarWidth: 'none', height: '100vh', paddingBottom: '200px',
+                    }}>
+
+                    <div className="flex justify-center">
+                        <img src='./images/signup.png' alt='confession' className='rounded-lg '
+                            width='auto' height='auto'
+                        />
+                    </div>
+                    <div className="flex flex-col gap-4 p-4">
+
+                        <input type="text" placeholder="Full Name" className="w-full p-4 border-2 border-gray-300 rounded-lg" />
+                        <input type="text" placeholder="Email" className="w-full p-4 border-2 border-gray-300 rounded-lg" />
+                        <input type="password" placeholder="Password" className="w-full p-4 border-2 border-gray-300 rounded-lg" />
+
+                        <button className="p-2 bg-blue-500 text-white rounded-lg">Sign Up</button>
+
+                    </div>
+
+
+
+
+                </div>
+
+
+            </div>
+            <div className='hidden md:block'>
+                <h1>Section 3</h1>
+            </div>
+        </div>
+    )
+}
