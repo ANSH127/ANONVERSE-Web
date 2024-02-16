3a1ed4844580cffd2a73b684f193d427a695fdac
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-02-17 00:30:01 +0530

    fetching userConfession

--- src/pages/YourConfessionScreen.js
+++ src/pages/YourConfessionScreen.js
@@ -4,9 +4,48 @@ import NavCard from '../components/NavCard';
 import ProfileCard from '../components/ProfileCard';
 import SearchSection from '../components/SearchSection';
 import TrendingCards from '../components/TrendingCards';
+import {confessionRef } from '../config/firebase';
+import { getDocs, where,query, orderBy } from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom';
+
 
 
 export default function YourConfessionScreen() {
+  const [confessions, setConfessions] = React.useState([])
+  const navigate = useNavigate()
+
+
+  const fetchConfessions = async () => {
+    try {
+
+      if (localStorage.getItem('user') === null) {
+        navigate('/login')
+      }
+      let uid=JSON.parse(localStorage.getItem('user')).uid
+      const q = query(confessionRef, where('uid', '==', uid), orderBy('createdAt', 'desc'))
+
+      const docSnap = await getDocs(q);
+      let data = []
+      docSnap.forEach((doc) => {
+        data.push({ ...doc.data(), id: doc.id })
+      });
+      setConfessions(data)
+      console.log(data)
+
+    } catch (error) {
+      console.error("Error getting documents: ", error);
+
+
+    }
+  }
+
+  React.useEffect(() => {
+    fetchConfessions();
+
+    // eslint-disable-next-line
+  }, [])
+
+
   return (
 
     <div className='grid-cols-1 grid md:grid-cols-4 gap-8  h-full p-4 m-2 fixed w-full' style={{ height: '100%' }}>
@@ -24,12 +63,17 @@ export default function YourConfessionScreen() {
           style={{
             scrollbarWidth: 'none', height: '100vh', paddingBottom: '200px',
           }}>
-          <Card />
-          <Card />
-          <Card />
+            {
+              confessions.map((data, index) => {
+                return (
+                  <Card key={index} data={data} />
+                )
+              })
+            
+            }
         </div>
       </div>
-      
+
       <div className='mr-2 hidden md:block'>
 
         <SearchSection />
