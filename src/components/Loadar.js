f6bf2f75870ecdf1d59db62ec820004c09cd2c67
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-02-16 17:19:01 +0530

    adding signup logic

---
+++ src/components/Loadar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Loadar() {
+  return (
+    <div>
+      <img src='./images/loadar.gif' alt='loading' className='w-12 h-12 mx-auto' />
+    </div>
+
+  )
+}
--- src/pages/SignUpScreen.js
+++ src/pages/SignUpScreen.js
@@ -2,9 +2,53 @@ import React from 'react'
 import NavCard from '../components/NavCard';
 import ProfileCard from '../components/ProfileCard';
 import { Link } from 'react-router-dom';
-
+import Loadar from '../components/Loadar';
+import { createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth'
+import { auth, usersRef } from '../config/firebase'
+import { addDoc } from 'firebase/firestore'
 
 export default function SignUpScreen() {
+    const [loading, setLoading] = React.useState(false)
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const [Name, setName] = React.useState('')
+
+    const handleSignUp = async () => {
+        if (email === '' || password === '' || Name === '') {
+            alert('All fields are required')
+            return
+        }
+        else {
+            setLoading(true)
+            try {
+                const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+                const user = userCredential.user
+                await sendEmailVerification(user)
+
+                await addDoc(usersRef, {
+                    name: Name,
+                    email: email,
+                    uid: user.uid,
+                    createdAt: new Date().toISOString(),
+                    avatar: 0
+                })
+
+
+                alert('Account created successfully. Please verify your email to login')
+                setEmail('')
+                setPassword('')
+                setName('')
+
+            } catch (error) {
+                alert(error.message)
+            }
+            finally {
+                setLoading(false)
+            }
+
+        }
+    }
+
     return (
 
         <div className='grid-cols-1 grid md:grid-cols-4 gap-8  h-full p-4 m-2 fixed w-full' style={{ height: '100%' }}>
@@ -25,15 +69,27 @@ export default function SignUpScreen() {
                     </div>
                     <div className="flex flex-col gap-4 p-4">
 
-                        <input type="text" placeholder="Full Name" className="w-full p-4 border-2 border-gray-300 rounded-lg" />
-                        <input type="text" placeholder="Email" className="w-full p-4 border-2 border-gray-300 rounded-lg" />
-                        <input type="password" placeholder="Password" className="w-full p-4 border-2 border-gray-300 rounded-lg" />
+                        <input type="text" placeholder="Full Name" className="w-full p-4 border-2 border-gray-300 rounded-lg"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <input type="text" placeholder="Email" className="w-full p-4 border-2 border-gray-300 rounded-lg"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input type="password" placeholder="Password" className="w-full p-4 border-2 border-gray-300 rounded-lg"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
 
                         <p className="text-center">Already have an account?
-                            <Link to='/signin' className="text-blue-500"> Sign In</Link>
+                            <Link to='/login' className="text-blue-500"> Sign In</Link>
                         </p>
 
-                        <button className="p-2 bg-blue-500 text-white rounded-lg">Sign Up</button>
+                        {
+                            loading ?
+                                <Loadar />
+                                :
+                                <button className="p-2 bg-blue-500 text-white rounded-lg"
+                                    onClick={handleSignUp}>Sign Up
+                                </button>}
 
                     </div>
 
