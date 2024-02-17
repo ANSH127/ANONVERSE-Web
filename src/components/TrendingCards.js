e50ee70e22fc82fd74e15193351bc753bd20dac5
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-02-17 11:29:47 +0530

    fetching trending confession

--- src/components/TrendingCards.js
+++ src/components/TrendingCards.js
@@ -1,6 +1,43 @@
 import React from 'react'
-
+import { confessionRef } from '../config/firebase';
+import { getDocs, query, orderBy, limit } from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom';
+import { formatDistance } from 'date-fns'
 export default function TrendingCards() {
+    const navigate = useNavigate()
+    const [confessions, setConfessions] = React.useState([])
+
+
+    const fetchConfessions = async () => {
+        try {
+            if (localStorage.getItem('user') === null) {
+                navigate('/login')
+            }
+
+            const q = query(confessionRef, orderBy('createdAt', 'desc'), orderBy('likes', 'desc'), orderBy('comments', 'desc'), limit(3));
+            const docSnap = await getDocs(q);
+            let data = []
+            docSnap.forEach((doc) => {
+                data.push({ ...doc.data(), id: doc.id })
+            });
+            setConfessions(data)
+
+        } catch (error) {
+            console.log(error.message);
+            console.error("Error getting documents: ", error);
+
+
+        }
+    }
+
+    React.useEffect(() => {
+        fetchConfessions();
+
+        // eslint-disable-next-line
+    }, [])
+
+
+
     return (
 
         <div className='shadow-lg p-4 my-4 h-full  bg-white rounded-lg'>
@@ -17,108 +54,48 @@ export default function TrendingCards() {
                     scrollBehavior: 'smooth',
                 }}>
 
-                <div className='shadow-lg  bg-white rounded-lg mb-5'>
-                    <div className='flex items-center justify-between'>
-                        <div className='flex items-center space-x-2'>
-                            <img
-                                src='./images/Avatar.jpg'
-                                alt='profile'
-                                className='rounded-full'
-                                width='40'
-                                height='40'
-                            />
-                            <div>
-                                <p className='font-semibold text-sm'>John Doe</p>
-                                {/* time */}
-                                <p className='text-gray-500 text-xs'>2 hours ago</p>
+                {
+                    confessions.map((confession, index) => (
+                        <div key={index} className='shadow-lg  bg-white rounded-lg mb-5'>
+                            <div className='flex items-center justify-between'>
+                                <div className='flex items-center space-x-2'>
+                                    <img
+                                        src='./images/Avatar.jpg'
+                                        alt='profile'
+                                        className='rounded-full'
+                                        width='40'
+                                        height='40'
+                                    />
+                                    <div>
+                                        <p className='font-semibold text-sm'>{confession.name}</p>
+                                        {/* time */}
+                                        <p className='text-gray-500 text-xs'>
+                                            {formatDistance(new Date(confession?.createdAt), new Date(), { addSuffix: true })}
+
+                                        </p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className='mt-2'>
+                                <p className='text-xs'>
+                                    {confession.description}
+                                </p>
+
+                                {/* // read more gray color text */}
+                                <p className='text-gray-500 text-xs '>Read More</p>
+
                             </div>
                         </div>
+                    ))
 
-                    </div>
-                    <div className='mt-2'>
-                        <p className='text-xs'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-                            voluptatibus, quibusdam, doloribus, quos officiis quia
-                            exercitationem quas voluptate quod aperiam
-                        </p>
-
-                        {/* // read more gray color text */}
-                        <p className='text-gray-500 text-xs '>Read More</p>
-
-                    </div>
-
-
-                </div>
-                <div className='shadow-lg  bg-white rounded-lg mb-5'>
-                    <div className='flex items-center justify-between'>
-                        <div className='flex items-center space-x-2'>
-                            <img
-                                src='./images/Avatar.jpg'
-                                alt='profile'
-                                className='rounded-full'
-                                width='40'
-                                height='40'
-                            />
-                            <div>
-                                <p className='font-semibold text-sm'>John Doe</p>
-                                {/* time */}
-                                <p className='text-gray-500 text-xs'>2 hours ago</p>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className='mt-2'>
-                        <p className='text-xs'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-                            voluptatibus, quibusdam, doloribus, quos officiis quia
-                            exercitationem quas voluptate quod aperiam
-                        </p>
-
-                        {/* // read more gray color text */}
-                        <p className='text-gray-500 text-xs '>Read More</p>
-
-                    </div>
-
-
-                </div>
-                <div className='shadow-lg  bg-white rounded-lg mb-5'>
-                    <div className='flex items-center justify-between'>
-                        <div className='flex items-center space-x-2'>
-                            <img
-                                src='./images/Avatar.jpg'
-                                alt='profile'
-                                className='rounded-full'
-                                width='40'
-                                height='40'
-                            />
-                            <div>
-                                <p className='font-semibold text-sm'>John Doe</p>
-                                {/* time */}
-                                <p className='text-gray-500 text-xs'>2 hours ago</p>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className='mt-2'>
-                        <p className='text-xs'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-                            voluptatibus, quibusdam, doloribus, quos officiis quia
-                            exercitationem quas voluptate quod aperiam
-                        </p>
-
-                        {/* // read more gray color text */}
-                        <p className='text-gray-500 text-xs '>Read More</p>
-
-                    </div>
-
-
-                </div>
+                }
 
 
 
             </div>
 
-            
+
         </div>
     )
 }
