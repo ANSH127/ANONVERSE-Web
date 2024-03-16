5ac8b0f42305ad4f4f68640c49394e68176ec979
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-03-16 15:06:37 +0530

    adding vercel analytics

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import AppNavigation from './nav/AppNavigation';
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <>
       <Provider store={store}>
         <AppNavigation />
+        <Analytics/>
 
       </Provider>
     </>
