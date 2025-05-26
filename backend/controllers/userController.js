7edeae6b0600d45f2830440eb147be9f434d71e1
Ansh Agarwal <anshagrawal12348@gmail.com>
2025-05-26 17:46:41 +0530

    feat: add updateUserAvatar functionality and route for updating user avatar

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,8 +11,8 @@ const loginUser = async (req, res) => {
     try {
         const user = await User.login(email, password)
         const token = createToken(user._id)
-        let { _id,username } = user
-        res.status(200).json({_id,email, token,username })
+        let { _id,username,avatar } = user
+        res.status(200).json({_id,email, token,username,avatar })
     } catch (err) {
         res.status(400).json({ message: err.message })
     }
@@ -29,10 +29,30 @@ const signupUser = async (req, res) => {
     }
 }
 
+const updateUserAvatar=async(req,res)=>{
+    const {avatar} = req.body
+    const userId = req.user._id
+    try {
+        const user = await User.findByIdAndUpdate(userId, { avatar }, { new: true });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ message: 'Avatar updated successfully', user });
+
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+
+    
+}
+
+
+
 
 
 
 module.exports = {
     loginUser,
-    signupUser
+    signupUser,
+    updateUserAvatar
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const UserControllers = require('../controllers/userController');
+const requireAuth = require('../middleware/requireAuth');
+
 // login route
 
 router.post('/login', UserControllers.loginUser);
@@ -9,5 +11,8 @@ router.post('/login', UserControllers.loginUser);
 // signup route
 
 router.post('/signup', UserControllers.signupUser);
+
+// update user avatar route
+router.patch('/update-avatar', requireAuth, UserControllers.updateUserAvatar);
 
 module.exports = router;
